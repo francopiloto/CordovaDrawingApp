4d8a8374a951360e1a1f87c32a0f7a13b9e0fb10
Franco Marques <francopilloto@gmail.com>
2018-06-28 10:08:59 -0400

    Finished the results page

--- www/js/draw.js
+++ www/js/draw.js
@@ -4,20 +4,37 @@ var buffer;
 var ctx;
 var isMouseDown;
 var isClick;
+var attempts;
+var timer;
 			
 $().ready(function() 
 {
-	// setup buttons behavior
+	setupButtonBehavior();
+	setupAppFlow();
+	initGraphics();
+	setupDrawingEvents();
+	setupDrawingControls();
+	startNewGame();
+});
+
+// setup UI behavior for all buttons
+function setupButtonBehavior()
+{
 	$('.button').on('mousedown', function()
 	{
 		var button = $(this);
 		
 		button.css('background-position','-' + button.css('width') + ' ' + button.css('background-position-y'));
 		button.find('span').css('top', button.hasClass('button-large') ? '17px' : '14px');
+		
+		if (button.hasClass('button-drawing'))
+		{ 
+			button.find('div').css('top', '104px');
+			button.find('svg').css('marginTop', '32px').css('marginLeft', '20px');
+		}
 	});
 	
-	$(window)
-	.on('mouseup', function()
+	$(window).on('mouseup', function()
 	{
 		$('.button').each(function()
 		{
@@ -25,68 +42,43 @@ $().ready(function()
 			
 			button.css('background-position','0 ' + button.css('background-position-y'));
 			button.find('span').css('top', button.hasClass('button-large') ? '9px' : '6px');
+			
+			if (button.hasClass('button-drawing'))
+			{ 
+				button.find('div').css('top', '97px');
+				button.find('svg').css('marginTop', '25px').css('marginLeft', '25px');
+			}
 		});
-	})
-	.on('resize', function()
-	{
-		// update canvas size to occupy the whole screen
-		var w = $(window).width();
-		var h = $(window).height();
-		
-		// avoid losing the drowing whe the canvas is resized
-		buffer.width = w;
-		buffer.height = h;
-		buffer.getContext('2d').drawImage(canvas, 0, 0);
-		
-		canvas.width  = w;
-		canvas.height = h;
-		canvas.getContext('2d').drawImage(buffer, 0, 0);
-		
-		ctx.lineWidth = 6;
 	});
-	
-	$('.canvas').hide();
-	$('.popup-wrapper').hide();
-	
+}
+
+function setupAppFlow()
+{
 	// slide down the card to show the new challenge
 	$('#new-card').click(function() {
-		$('.card-container').animate({top: '0'}, 400);
+		newChallenge();
 	});
 	
 	// slide up the card and show the canvas
 	$('#gotit').click(function()
 	{
 		$('.home').hide();
-		$('.canvas').show();
-		
-		$('.card-container').animate({top: '-100%'}, 400);
-	});
-	
-	$('#button-clear').on('click', function() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-	});
-	
-	$('#button-skip').on('click', function() {
-		$('.card-container').animate({top: '0'}, 400);
-	});
-	
-	$('.button-close').on('click', function() {
-		$('.popup-wrapper').show();
+		startNewDrawing();
+		slideUp('.card-challenge');
 	});
 	
 	$('#popup-quit-cancel').on('click', function() {
 		$('.popup-wrapper').hide();
 	});
 	
-	$('#popup-quit-quit').on('click', function() 
-	{
-		$('.popup-wrapper').hide();
-		$('.canvas').hide();
-		$('.home').show();
+	$('#popup-quit-quit').on('click', function() {
+		startNewGame();
 	});
 	
-	initGraphics();
-});
+	$('#button-timesup-play').on('click', function() {
+		startNewGame();
+	});
+}
 
 // initialize graphics context and events
 function initGraphics()
@@ -100,7 +92,10 @@ function initGraphics()
 	
 	ctx.fillStyle = ctx.strokeStyle = '#000000';	
 	ctx.lineJoin = 'round';
-	
+}
+
+function setupDrawingEvents()
+{
 	$(canvas).on('mousedown', function(e)
 	{
 		var pos = getMousePosition(e);
@@ -146,7 +141,51 @@ function initGraphics()
 			ctx.arc(pos.x, pos.y, ctx.lineWidth, 0, Math.PI * 2, true);
 			ctx.fill();
 		}
-	}).resize();
+	})
+	.on('resize', function()
+	{
+		// update canvas size to occupy the whole screen
+		var w = $(window).width();
+		var h = $(window).height();
+		
+		// avoid losing the drowing whe the canvas is resized
+		buffer.width = w;
+		buffer.height = h;
+		buffer.getContext('2d').drawImage(canvas, 0, 0);
+		
+		canvas.width  = w;
+		canvas.height = h;
+		canvas.getContext('2d').drawImage(buffer, 0, 0);
+		
+		ctx.lineWidth = 6;
+	})
+	.resize();
+}
+
+function setupDrawingControls()
+{
+	$('#button-clear').on('click', function() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	});
+	
+	$('#button-skip').on('click', function() {
+		newChallenge();
+	});
+	
+	$('.button-close').on('click', function() {
+		$('.popup-wrapper').show();
+	});
+}
+
+function startNewGame()
+{
+	attempts = 0;
+	
+	$('#button-skip').show();
+	$('.card-container').hide();
+	$('.canvas').hide();
+	$('.popup-wrapper').hide();
+	$('.home').show();
 }
 
 // get current mouse position on canvas
@@ -158,4 +197,78 @@ function getMousePosition(e)
 		x: e.clientX - rect.left - canvas.clientLeft,
 		y: e.clientY - rect.top - canvas.clientTop
 	};
+}
+
+function newChallenge()
+{
+	++attempts;
+	
+	if (attempts > 6) 
+	{
+		showResults();
+		return;
+	}
+	
+	$('.card-challenge .level').html('Drawing ' + attempts + '/6');
+	
+	// TODO get challenge from the database
+	var challenge = 'new challenge from database';
+	
+	$('.challenge').html(challenge);
+	$('.canvas .topbar #topbar-text').html('Draw: ' + challenge);
+		
+	slideDown('.card-challenge');
+}
+
+function startNewDrawing()
+{
+	if (timer) {
+		clearInterval(timer);
+	}
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	isMouseDown = false;
+	isClick = false;
+	
+	if (attempts >= 6) {
+		$('#button-skip').hide();
+	}
+
+	$('.canvas').show();
+	
+	var counter = 20;
+	$('#clock-time').html('00:20');
+	
+	timer = setInterval(function()
+	{
+		--counter;
+		
+		if (counter > 0) {
+			$('#clock-time').html('00:' + (counter > 9 ? counter : '0' + counter));
+		}
+		else 
+		{
+			clearInterval(timer);
+			newChallenge();
+		}
+		
+	}, 1000);
+}	
+
+function slideDown(card) {
+	$(card).css('top','-100%').show().animate({top: '0'}, 400);
+}
+
+function slideUp(card)
+{
+	$(card).animate({top: '-100%'}, 400, function() {
+		$(card).hide();
+	});
+}
+
+function showResults()
+{
+	// TODO
+	
+	slideDown('.card-results');
 }
